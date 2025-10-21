ff380b819d408fcddd0c333aa9fbc4e0556a4bed
Samuel Li <samuel.li@utexas.edu>
2025-10-21 04:40:02 -0500

    airlift data textgrad

--- airlift-starter-kit/solution/mysolution.py
+++ airlift-starter-kit/solution/mysolution.py
@@ -7,6 +7,25 @@ from airlift.solutions import Solution
 
 import json
 
+class MySolution(Solution):
+    """
+    Utilizing this class for your solution is required for your submission. The primary solution algorithm will go inside the
+    policy function.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def reset(self, obs, observation_spaces=None, action_spaces=None, seed=None):
+        # Currently, the evaluator will NOT pass in an observation space or action space (they will be set to None)
+        super().reset(obs, observation_spaces, action_spaces, seed)
+
+        # Create an action helper using our random number generator
+        self._action_helper = ActionHelper(self._np_random)
+
+    def policies(self, obs, dones, infos):
+        # Use the acion helper to generate an action
+        return self._action_helper.sample_valid_actions(obs)
+
 def airlift_json_to_pddl(json_file, problem_out="../../database/problem.pddl"):
     with open(json_file, "r") as f:
         data = json.load(f)
